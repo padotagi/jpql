89d7f5409999a60268f9ae3eb9cf3fbb5b42e715
Seine <>
2024-07-10 17:02:56 +0900

    20240710 grammar

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//            Query query3 = em.createQuery("select m.username, m.age from Member m"); //타입 정보를 받을 수 없을 때
+
+            List<Member> resultList = query.getResultList(); // query.getSingleList() : 1개 안 나오면 Exception 발생
+
+            // qquery.setParameter("username", "member1")
+            // 포지션 방식 비추
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
